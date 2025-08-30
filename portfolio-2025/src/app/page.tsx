809433c80e0999c3bbcd090183850b875b654d85
Pedro Lima <pedrohblima03@gmail.com>
2025-08-29 23:48:46 -0300

    feat: add aboutme section

--- portfolio-2025/src/app/page.tsx
+++ portfolio-2025/src/app/page.tsx
@@ -1,3 +1,4 @@
+import AboutMe from "@/components/AboutMe";
 import Header from "@/components/Header";
 import { Hero } from "@/components/Hero";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <Hero />
+      <AboutMe />
     </>
   );
 }
---
+++ portfolio-2025/src/components/AboutMe/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Container from '../Container'
+
+const AboutMe = () => {
+  return (
+    <section id="about-me" className='py-16'>
+      <Container>
+        <div className='eyebrow mb-20'>
+          <p className='text-4xl md:text-6xl font-light'>
+            My approach is driven by user-centered design, ensuring that each product is not just functional, but truly resonates with its audience.
+          </p>
+        </div>
+
+        <div className='main-content'>
+          <h2 className='pb-3 border-b text-muted-foreground border-b-muted-foreground'>Get to know me</h2>
+          <div className='grid grid-cols-12 gap-4 pt-8'>
+            <div className='col-span-12 md:col-span-5'>
+              <h3 className='text-4xl'>Hi, I&apos;m Pedro</h3>
+            </div>
+            <div className='col-span-12 md:col-span-7'>
+              <p>
+                Having worked as a software developer for five years, I consider myself passionate about creating innovative solutions that positively impact people's lives. I have experience writing clean, efficient, and high-performance code, always striving for simplicity and quality in software development. I currently prioritize opportunities that can contribute to my learning and growth and that make sense for improving people&apos;s lives.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default AboutMe
